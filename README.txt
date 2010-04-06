8004a0ab52b5be890e0a90c9c6a00d6788e582a2
Jacob Kaplan-Moss <jacob@jacobian.org>
2010-04-06 12:01:17 -0500

    Seems to work.

---
+++ README.txt
@@ -0,0 +1,11 @@
+Python bindings to the Evernote API.
+
+All the code comes direct from Evernote -- see
+http://www.evernote.com/about/developer/api/ -- and is owned by them.
+
+This is simply an unofficial package produced from that code and uploaded to
+PyPI. Questions about using the Evernote APIs should go to them;
+questions/problems about the packaging should go to me.
+
+My additions -- the setup.py, the fabfile to automate production of the package,
+this readme -- are BSD licensed; see LICENSE.txt.
---
+++ fabfile.py
@@ -0,0 +1,38 @@
+import urllib
+import BeautifulSoup
+from contextlib import nested
+from fabric.api import *
+from unipath import FSPath as Path
+
+API_LANDING_PAGE = 'http://www.evernote.com/about/developer/api/'
+API_LINK_TITLE = 'Evernote API'
+BASE_PATH = Path(__file__).absolute().parent
+
+def sdist():
+    """
+    Build a sdist from the latest Evernote Python API wrappers.
+    """
+    landing_page = BeautifulSoup.BeautifulSoup(urllib.urlopen(API_LANDING_PAGE))
+    api_link = landing_page.find('a', text=API_LINK_TITLE).parent['href']
+    api_zip_name = Path(api_link).name
+    api_zip_dest = BASE_PATH.child(api_zip_name)
+
+    if not api_zip_dest.exists():
+        local('curl -O "%s"' % api_link)
+        
+    unziped_dir = BASE_PATH.child(api_zip_name.stem)
+    if not unziped_dir.exists():
+        local('unzip %s' % api_zip_name)
+    
+    local('cp -r -f %s/lib/python/evernote evernote' % unziped_dir)
+    
+    with nested(open('setup.py.in'), open('setup.py', 'w')) as (fin, fout):
+        fout.write(fin.read() % {'version': unziped_dir.stem.split('-')[-1]})
+        
+    local('python setup.py sdist')
+    
+def clean():
+    """
+    Clean up any leftovers.
+    """
+    local('rm -rf dist evernote* setup.py')
